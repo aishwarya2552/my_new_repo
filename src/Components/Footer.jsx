ce6b532cd270aa3b66ee4be34721a049a024a5ce
aishwarya2552 <145648691+aishwarya2552@users.noreply.github.com>
2024-07-03 10:16:18 +0000

    name new changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -61,7 +61,7 @@ const Footer = (props) => {
         }}
       >
         {email && (
-          <a href={`mailto:${email}`}>
+          <a href={`mailto:aishsrivastava41@gmail.com`}>
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
@@ -71,13 +71,13 @@ const Footer = (props) => {
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/aishwarya2552`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
         {instagram && (
           <a
-            href={`https://www.instagram.com/${instagram}`}
+            href={`https://www.instagram.com/aishwarya_srivastava_as/?next=%2F`}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -86,7 +86,7 @@ const Footer = (props) => {
         )}
         {linkedIn && (
           <a
-            href={`https://www.linkedin.com/in/${linkedIn}`}
+            href={`https://www.linkedin.com/in/aishwarya-srivastava-433b63257/`}
             target="_blank"
             rel="noopener noreferrer"
           >
